0038e52da699cff4c635ec3d643c884869a277d5
Aishwarya Dhiman <43997524+AishwaryaDhiman@users.noreply.github.com>
2020-10-30 13:47:44 +0530

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+        Global Nettech an leading Laptop Repair Service Center in Bangalore, have professionally trained on Laptop technicians that offer repair & support services in Chennai, Hyderabad & Cochin +91 90360 10005 Mumbai, Pune, Delhi, Noida & Gurgaon. All the Laptop repairs and services are done by the well-trained technicians so that our customers will receive the same quality of service which they get from the apple stores.
+        </p>
+
+        <p className="lead">
+          .<br></br>
+        Free Doorstep/Onsite Service
+ 
+
+ Call +91 99999999, Global Nettech Can repair Your Laptop At Your Convenient Place. We Have Qualified & Experienced Technicians We Use Genuine & Original Spare Parts Express Laptop Repair in Bangalore (Mostly in less than 2 hours) 1 year or 6 Months Warranty On Selected Repairs & Services Reasonable Charges For Laptop Repairs & Services.
+        </p>
+        <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt nam accusantium? Soluta cupiditate, accusamus commodi praesentium laborum dolorum libero maiores!</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <div>
+      
+      <h1>
+      <br></br><br></br>
+      WELCOME TO  <br>
+      </br>A.D. Laptop Warranty Service
+      </h1>
+     
+    </div>
+      
+)};
+
+export default Home;
